53380cd14771fc43f5460ba940d399aa25155597
jiangangzou <38418733+jiangangzou@users.noreply.github.com>
2018-06-25 10:48:38 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <el-button loading
+    type="success">
+      提交
+    </el-button>
+    <el-button
+    @click="doLoading"
+    :loading="isLoading"
+    icon="el-icon-edit"
+    type="primary">
+    
+    </el-button>
+    <el-button :disabled="disabled"
+    type="info">
+      提交
+    </el-button>
+    <el-button :disabled="disabled"
+    type="warning">
+      提交
+    </el-button>
+    <el-button :disabled="disabled"
+    type="danger">
+      提交
+    </el-button>
+  </div>
+</template>
+
+<script>
+import ElButton from '@/components/ElButton'
+export default {
+  name: 'App',
+  data () {
+    return {
+      disabled: false,
+      isLoading: false
+    }
+  },
+  components: {
+    'el-button': ElButton
+  },
+  mounted () {
+    setTimeout(() => {
+      this.disabled = true;
+    }, 2000);
+  },
+  methods: {
+    doLoading () {
+      console.log('aaa');
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ElButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button
+  class="el-button"
+  :class = "[
+    type? 'el-button--' + type : '',
+    {'is-disabled': buttonDisabled,
+    'is-loading': loading}
+  ]"
+  :disabled="buttonDisabled">
+    <i class="el-icon-loading" v-if="loading"></i>
+    <i v-if="icon&&!is-loading" :class="icon"></i>
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  name: 'ElButton',
+  props: {
+    disabled: Boolean,
+    type: {
+      type: String,
+      default: 'default'
+    },
+    loading: Boolean,
+    icon: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    buttonDisabled () {
+      return this.disabled;
+    }
+  }
+}  
+</script>
+<style>
+
+</style>
